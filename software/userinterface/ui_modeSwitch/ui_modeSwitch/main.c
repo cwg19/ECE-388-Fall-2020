5ebbb4975f2c31958e3c9923a411589588bbe276
cwg19 <47038555+cwg19@users.noreply.github.com>
2020-10-12 00:03:08 -0400

    Updating UI solution
    Rotary encoder interfacing is successful. Have yet to separate functionality into header and implementation files for lcd and rotary encoder functionality. The main program of this project is a test, however the main of my working project will have a similar outline to this; the difference mainly being that in the main switch case instead of incrementing a counter, registers in the AD9833 and MAX9939 will be set to adjust the signal, frequency, phase and gain as needed. HAVE NOT TESTED THE LCD PORTION OF CODE YET. NEED LCD/I2C backpack. It has worked in the past... probably effed something in it. It does build.

--- software/userinterface/ui_modeSwitch/ui_modeSwitch/main.c
+++ software/userinterface/ui_modeSwitch/ui_modeSwitch/main.c
@@ -6,18 +6,28 @@
  */ 
 
 #include <avr/io.h>
-#define F_CPU 16000000
+#include <stdlib.h>
+#define F_CPU 16000000UL
 #include <util/delay.h>
 #include "lcd.h"
 
 extern void lcd_backlight(char on); 
-uint8_t getRotaryStatus(void);
+void setRotaryStatus(void);
+void getNextMode(void);
+void getPreviousMode(void);
+void clearScreen(void);
+void uiInit(void);
+void clearLine(unsigned char y);
+void clearSpace(unsigned char x1,unsigned char x2, unsigned char y);
+void displayRefresh(void);
+
 
 /* modes
 	1. signal mode display 
 	2. voltage display
 	3. frequency display
 	4. phase display
+	In each mode display the mode and the appro
 */
 
 // I2C
@@ -34,75 +44,122 @@ uint8_t getRotaryStatus(void);
 #define ROTARY_A PINC0
 #define ROTARY_B PINC1
 #define ROTARY_BUTTON PINC2
+#define NO_ROTATION 0
+#define CLOCKWISE 1
+#define COUNTERCLOCKWISE 2
+#define SET_VAL_FLAG 1
+
+#define FREQ_MAX 1000000UL
+#define FREQ_MIN 1
+
+#define PHASE_MAX 180
+#define PHASE_MIN  -180
+
+#define V_MAX 20
+#define V_MIN -20
 
 #define MODE_SIGNAL 0
 #define MODE_VOLTAGE 1
 #define MODE_FREQUENCY 2
 #define MODE_PHASE 3
 
+#define SIGNAL_SIN 0
+#define SIGNAL_COS 1
+#define SIGNAL_SQ 2
+#define SIGNAL_TRI 3
+
 uint8_t mode = 0;
-uint8_t count = 0;
+uint8_t modeLast = 0xFF;
+uint8_t setVal = 0;
+uint8_t aQuiescent = 0;
+uint8_t aState = 0;
+uint8_t rotation = 0;
+
+const char* labels[4] = {"Signal:","Voltage:","Frequency:","Phase:"};
+const char* units[3] = {" V"," Hz"," deg"};
+const char* signals[4] = {"sine","cosine","square","triangle"};
+
+uint32_t frequency = 0;
+int16_t phase = 0;
+int8_t voltage = 0;
+uint8_t signal = 0;
 
 
 
-int main(void)
-{
-	ROTARY_DDR &= ~((1<<ROTARY_A) | (1<<ROTARY_B) | (1<<ROTARY_BUTTON));
-	ROTARY_PORT |= (1<<ROTARY_A) | (1<<ROTARY_B) | (1<<ROTARY_BUTTON);
-	uint8_t aLastState = ROTARY_PIN & (1<<ROTARY_A);
-	uint8_t aState = 0;
-    while (1) 
-    {
-		aState = ROTARY_PIN & (1<<ROTARY_A);
-		if (aState != aLastState) {
-			if ((ROTARY_PIN & (1<<ROTARY_B)) != aState) {
-				count++;
-			}
-			else {
-				count--;
+int main(void) {
+	ROTARY_DDR &= ~((1<<ROTARY_A) | (1<<ROTARY_B) | (1<<ROTARY_BUTTON)); // encoder pins and button set as input
+	ROTARY_PORT |= (1<<ROTARY_A) | (1<<ROTARY_B); // set pull-up resistors on encoder pins.
+	aQuiescent = ROTARY_PIN & (1<<ROTARY_A);
+	uiInit();
+    while (1) {
+		setRotaryStatus();
+		if (setVal) {
+			switch(mode) {
+				case MODE_SIGNAL:
+					if (rotation == CLOCKWISE)
+						signal = (signal+1)&3;
+					else if (rotation == COUNTERCLOCKWISE)
+						signal = (signal-1)&3;
+					break;
+				case MODE_VOLTAGE:
+					if ((rotation == CLOCKWISE) && (voltage < V_MAX)) {
+						voltage++;
+					}
+					else if ((rotation == COUNTERCLOCKWISE) && (voltage > V_MIN)) {
+						voltage--;
+					}
+					break;
+				case MODE_FREQUENCY:
+					if ((rotation == CLOCKWISE) && (frequency < FREQ_MAX))
+						frequency++;
+					else if ((rotation == COUNTERCLOCKWISE) && (frequency > FREQ_MIN))
+						frequency--;
+					break;
+				case MODE_PHASE:
+					if ((rotation == CLOCKWISE) && (phase < PHASE_MAX))
+						phase++;
+					else if ((rotation == COUNTERCLOCKWISE) && (phase > PHASE_MIN))
+						phase--;
+					break;
 			}
 		}
-
-// 		switch(mode) {
-// 			case MODE_SIGNAL:
-// 				break;
-// 			case MODE_VOLTAGE:
-// 				break;
-// 			case MODE_FREQUENCY:
-// 				break;
-// 			case MODE_PHASE:
-// 				break;
-// 			case default:
-// 				break;
-     }
+		else {
+			if (rotation == CLOCKWISE)
+				getNextMode();
+			else if (rotation == COUNTERCLOCKWISE)
+				getPreviousMode();
+		}
+		// display mode and value with units
+		if (rotation != NO_ROTATION) {
+			displayRefresh();
+			_delay_ms(50);
+		}
+	}
 }
 
 
 // rotary encoder 
-uint8_t nextMode(uint8_t currentMode) {
-	uint8_t next;
-	return next = (currentMode+1)&3;
+void getNextMode(void) {
+	modeLast = mode;
+	mode = (mode+1)&3;
 }
 
-uint8_t previousMode(uint8_t currentMode) {
-	uint8_t prev;
-	return prev = (currentMode-1)&3;
+void getPreviousMode(void) {
+	modeLast = mode;
+	mode = (mode-1)&3;
 }
 
-uint8_t getRotaryStatus(void) {
-	uint8_t status = 0;
-	if (!(ROTARY_PIN & (1<<ROTARY_A))) {
-		while(ROTARY_PIN&(1<<ROTARY_B));
-		status = 1;
-	}
-	if (!(ROTARY_PIN & (1<<ROTARY_B))) {
-		while(ROTARY_PIN&(1<<ROTARY_A));
-		status = 2;
-	}
-	return status;
+void setRotaryStatus(void) {
+	aState = ROTARY_PIN & (1<<ROTARY_A);
+	rotation = NO_ROTATION;
+	if (aState != aQuiescent)
+		rotation = (ROTARY_PIN & (1<<ROTARY_B)) != aState ? COUNTERCLOCKWISE : CLOCKWISE;
+	if (ROTARY_PIN & (1<<ROTARY_BUTTON))
+		setVal ^= SET_VAL_FLAG;
 }
 
 // lcd
+
 void clearScreen(void) {
 	lcd_clrscr();
 	_delay_ms(10);
@@ -114,7 +171,8 @@ void uiInit(void) {
 	_delay_ms(500);
 	lcd_backlight(1);
 	_delay_ms(500);
-	clearScreen();
+	//clearScreen();
+	displayRefresh();
 }
 
 void clearLine(unsigned char y) {
@@ -130,3 +188,25 @@ void clearSpace(unsigned char x1,unsigned char x2, unsigned char y) {
 	lcd_gotoxy(x1,y);
 }
 
+void displayRefresh(void) {
+	char val[10];
+	if (mode != modeLast) {
+		clearScreen();
+		lcd_puts(labels[mode]);
+	}
+	else
+		clearLine(1);
+	lcd_gotoxy(0,1);
+	if (mode == MODE_SIGNAL)
+		lcd_puts(signals[signal]);
+	else {
+		if (mode == MODE_VOLTAGE)
+			itoa(voltage,val,10);
+		else if (mode == MODE_FREQUENCY)
+			itoa(frequency,val,10);
+		else if (mode == MODE_PHASE)
+			itoa(phase,val,10);
+		lcd_puts(val);
+		lcd_puts(units[mode-1]);
+	}
+}
